55a5545da1227b5edbd0d7faa8d5bbf7ec3946af
AngelaRebancosBautista <angelarbautista22@gmail.com>
2025-10-26 12:34:16 +0800

    Organizing Containers of Balls.cs

---
+++ Organizing Containers of Balls.cs
@@ -0,0 +1,84 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'organizingContainers' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts 2D_INTEGER_ARRAY container as parameter.
+     */
+
+    public static string organizingContainers(List<List<int>> container)
+    {
+         int n = container.Count;
+
+        List<long> containerCapacities = new List<long>();
+        // Total number of balls of each type
+        List<long> typeCounts = new List<long>(new long[n]);
+
+        for (int i = 0; i < n; i++)
+        {
+            long rowSum = 0;
+            for (int j = 0; j < n; j++)
+            {
+                rowSum += container[i][j];
+                typeCounts[j] += container[i][j];
+            }
+            containerCapacities.Add(rowSum);
+        }
+
+        containerCapacities.Sort();
+        typeCounts.Sort();
+
+        for (int i = 0; i < n; i++)
+        {
+            if (containerCapacities[i] != typeCounts[i])
+                return "Impossible";
+        }
+
+        return "Possible";
+    }
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int q = Convert.ToInt32(Console.ReadLine().Trim());
+
+        for (int qItr = 0; qItr < q; qItr++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<List<int>> container = new List<List<int>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                container.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(containerTemp => Convert.ToInt32(containerTemp)).ToList());
+            }
+
+            string result = Result.organizingContainers(container);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
